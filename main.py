05287e59bdfd3317a4eb45008c64595dc6f3d98f
juwonjeong0123 <juwonjeong0123@gmail.com>
2025-08-28 13:54:10 +0900

    sans

--- main.py
+++ main.py
@@ -1,8 +1,44 @@
-# 상수
+# ====================
+# 라이브러리
+# ====================
+import math
 
-#Class
+# ====================
+#   상수
+# ====================
+G = 6.674e-11
+
+# ====================
+#   class
+# ====================
+class Vector2:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
 class CelestialBody:
-    def __init__(self, name):
-        self.name = name,
+    def __init__(self, name, mass, radius, pos, v, color):
+        self.name = name
+        self.mass = mass
+        self.radius = radius
+        self.pos = pos
+        self.v = v
+        self.color = color
+
+    def get_gravity(self, m1, m2):
+        pass
+
+    def get_distance(self, pos1, pos2):
+        result = 0
+
+        x1 = pos1.x
+        y1 = pos1.y
+        x2 = pos2.x
+        y2 = pos2.y
+
+        result = math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+        return result
+
 
 
